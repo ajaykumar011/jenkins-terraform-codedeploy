04a1075f85f816be2770fc785e75b08d6f8bcd12
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-18 14:09:36 +0000

    update

--- codedeploy.tf
+++ codedeploy.tf
@@ -1,27 +1,43 @@
 # create a CodeDeploy application
 resource "aws_codedeploy_app" "main" {
-  name = "Sample_App"
+  name = "Nginx_Web_App"
 }
 
 # create a deployment group
 resource "aws_codedeploy_deployment_group" "main" {
   app_name              = aws_codedeploy_app.main.name
-  deployment_group_name = "Sample_DepGroup"
+  deployment_group_name = "Nginx_Web_DepGroup"
   service_role_arn      = aws_iam_role.codedeploy_service.arn
-
   deployment_config_name = "CodeDeployDefault.OneAtATime" # AWS defined deployment config
+  autoscaling_groups     = aws_autoscaling_group.app-launchtp-asg.name
+  deployment_style {
+    deployment_option = WITHOUT_TRAFFIC_CONTROL
+    deployment_type = IN_PLACE
+  }
+
+  load_balancer_info{
+    target_group_info  {
+      name = aws_lb_target_group.app-alb-tg1.name
+    }
+  }
 
   ec2_tag_set {
     ec2_tag_filter {
-      key   = "filterkey1"
+      key   = "OS"
       type  = "KEY_AND_VALUE"
-      value = "filtervalue"
+      value = "Ubuntu"
     }
 
     ec2_tag_filter {
-      key   = "filterkey2"
+      key   = "Platform"
       type  = "KEY_AND_VALUE"
-      value = "filtervalue"
+      value = "Dev"
+    }
+
+    ec2_tag_filter {
+      key   = "Provisioner"
+      type  = "KEY_AND_VALUE"
+      value = "Terraform"
     }
   }
 
