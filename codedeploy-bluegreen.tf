795fd24ec970459b9fe0b8f34fdb5d82fe2fc434
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-18 16:09:04 +0000

    update

--- codedeploy-bluegreen.tf
+++ codedeploy-bluegreen.tf
@@ -1,3 +1,4 @@
+#Disable codedeploy-inplace.tf and enable alb.tf to use this.
 # create a CodeDeploy application
 resource "aws_codedeploy_app" "main" {
   name = "Nginx_Web_App"
@@ -8,19 +9,35 @@ resource "aws_codedeploy_deployment_group" "main" {
   app_name              = aws_codedeploy_app.main.name
   deployment_group_name = "Nginx_Web_DepGroup"
   service_role_arn      = aws_iam_role.codedeploy_service.arn
-  deployment_config_name = "CodeDeployDefault.OneAtATime" # AWS defined deployment config
-  autoscaling_groups     = [aws_autoscaling_group.app-launchtp-asg.name]
+  //deployment_config_name = "CodeDeployDefault.OneAtATime" # AWS defined deployment config
+ // autoscaling_groups     = [aws_autoscaling_group.app-launchtp-asg.name]
+
   deployment_style {
     deployment_option = "WITH_TRAFFIC_CONTROL"
-    deployment_type = "IN_PLACE"
+    deployment_type = "BLUE_GREEN"
   }
 
   load_balancer_info {
-    target_group_info  {
-      name = aws_lb_target_group.app-alb-tg1.name
+    elb_info {
+      name = aws_lb_target_group.app-alb-tg1.name-------------
     }
   }
 
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout    = "STOP_DEPLOYMENT"
+      wait_time_in_minutes = 60
+    }
+
+    green_fleet_provisioning_option {
+      action = "DISCOVER_EXISTING"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action = "KEEP_ALIVE"
+    }
+  }
+  
   ec2_tag_set {
     ec2_tag_filter {
       key   = "OS"
