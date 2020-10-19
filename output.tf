f91e445c7adbb77270cff3d0542460fa3b026c12
Ajay Kumar <ajkumar011.sharma@hotmail.com>
2020-10-19 02:09:57 +0000

    update

--- output.tf
+++ output.tf
@@ -1,6 +1,8 @@
-output "ELB" {
+#ALB
+output "ALB" {
   value = aws_lb.app-alb.dns_name
 }
+
 
 output bucket_name {
   value = aws_s3_bucket.b.bucket_domain_name
--- s3.tf
+++ s3.tf
@@ -28,3 +28,36 @@ resource "random_integer" "suffix" {
   min = 100
   max = 999
 }
+
+
+
+#########################
+# S3 bucket for ELB logs SAMPLE
+#########################
+// data "aws_elb_service_account" "this" {}
+
+// resource "aws_s3_bucket" "logs" {
+//   bucket        = "elb-logs-${random_pet.this.id}"
+//   acl           = "private"
+//   policy        = data.aws_iam_policy_document.logs.json
+//   force_destroy = true
+// }
+
+// data "aws_iam_policy_document" "logs" {
+//   statement {
+//     actions = [
+//       "s3:PutObject",
+//     ]
+
+//     principals {
+//       type        = "AWS"
+//       identifiers = [data.aws_elb_service_account.this.arn]
+//     }
+
+//     resources = [
+//       "arn:aws:s3:::elb-logs-${random_pet.this.id}/*",
+//     ]
+//   }
+// }
+
+##################
